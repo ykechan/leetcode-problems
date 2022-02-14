b74b1bd58d2a5b6dffb81cd8cb765a3a046cfbd4
ykechan <ykechan@gmail.com>
2022-02-14 22:06:37 +0800

    Initial commit

---
+++ src/main/java/com/leetcode/ykechan/LeetcodeProblemsApplication.java
@@ -0,0 +1,13 @@
+package com.leetcode.ykechan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeProblemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeProblemsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leetcode/ykechan/LeetcodeProblemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.ykechan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeProblemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
