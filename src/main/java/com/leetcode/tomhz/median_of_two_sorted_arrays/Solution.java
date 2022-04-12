678e6089d11a17c40fc0d822af5007429c46d4a7
tomHz8 <103101761+tomHz8@users.noreply.github.com>
2022-04-12 16:59:10 +0800

    update leetCode4_median_of_two_sorted_arrays
    https://leetcode.com/problems/median-of-two-sorted-arrays/

---
+++ src/main/java/com/leetcode/tomhz/median_of_two_sorted_arrays/Solution.java
@@ -0,0 +1,76 @@
+package leetCode4_median_of_two_sorted_arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		Set<Integer> set = new HashSet<>();
+        int length = nums1.length + nums2.length;
+		int a = 0,b = 0;
+		int rs = 0;
+		set.add(length / 2); 
+		if(length % 2 == 0) {
+			set.add(length / 2 - 1); 
+		}
+		while(!set.isEmpty()) {
+			int tmp;
+			if(a >= nums1.length) {
+				if(set.contains(a + b)) {
+					rs += nums2[b];
+					set.remove(a + b);
+				}
+				b++;
+				continue;
+			}
+			if(b >= nums2.length) {
+				if(set.contains(a + b)) {
+					rs += nums1[a];
+					set.remove(a + b);
+				}
+				a++;
+				continue;
+			}
+			
+			if(nums1[a] > nums2[b]) {
+				if(set.contains(a + b)) {
+					rs += nums2[b];
+					set.remove(a + b);
+				}
+				b++;
+			}else {
+				if(set.contains(a + b)) {
+					rs += nums1[a];
+					set.remove(a + b);
+				}
+				a++;
+			}
+		}
+		return length % 2 == 0 ? rs / 2.0 : rs;
+    }
+	
+//	public int binarySearch(int arr[], int l, int r, int x)
+//    {
+//        if (r >= l) {
+//            int mid = l + (r - l) / 2;
+// 
+//            // If the element is present at the
+//            // middle itself
+//            if (arr[mid] == x)
+//                return mid;
+// 
+//            // If element is smaller than mid, then
+//            // it can only be present in left subarray
+//            if (arr[mid] > x)
+//                return binarySearch(arr, l, mid - 1, x);
+// 
+//            // Else the element can only be present
+//            // in right subarray
+//            return binarySearch(arr, mid + 1, r, x);
+//        }
+// 
+//        // We reach here when element is not present
+//        // in array
+//        return -1;
+//    }
+}
